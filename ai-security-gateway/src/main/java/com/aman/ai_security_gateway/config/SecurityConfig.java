73e99e3b2b52edfaa8714541ed168e6de783e31c
AMAN KATIYAR <warlord@AMANs-MacBook-Air.local>
2026-06-28 15:05:30 +0530

    Dockerize frontend and allow request from docker

--- ai-security-gateway/src/main/java/com/aman/ai_security_gateway/config/SecurityConfig.java
+++ ai-security-gateway/src/main/java/com/aman/ai_security_gateway/config/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:5173",
+                        "http://localhost:3000")
         );
 
         configuration.setAllowedMethods(
