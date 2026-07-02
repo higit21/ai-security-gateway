1ca28bfcf9bfa05c207e99fbaa0e9abf5789ce5c
AMAN KATIYAR <warlord@AMANs-MacBook-Air.local>
2026-07-02 23:46:55 +0530

    Update url in backend to accept req from frontend

--- ai-security-gateway/src/main/java/com/aman/ai_security_gateway/config/SecurityConfig.java
+++ ai-security-gateway/src/main/java/com/aman/ai_security_gateway/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(
                 List.of("http://localhost:5173",
-                        "http://localhost:3000")
+                        "http://localhost:3000",
+                        "http://13.63.236.228:3000")
         );
 
         configuration.setAllowedMethods(
